2c810e22ac00c7bd01efc8576696fa4d584698f0
Ashutosh Jha <ashutosh1@veersatech.com>
2026-06-15 16:56:11 +0530

    chatbot with history context

--- 02_LangChain_Prompts/messages.py
+++ 02_LangChain_Prompts/messages.py
@@ -1,11 +1,19 @@
 from langchain.chat_models import init_chat_model
 from langchain.messages import HumanMessage, AIMessage, SystemMessage
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
 
-model = init_chat_model("gpt-5-nano")
+load_dotenv()
+
+llm = HuggingFaceEndpoint (
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm) 
 
 system_msg = SystemMessage("You are a helpful assistant.")
 human_msg = HumanMessage("Hello, how are you?")
 
-# Use with chat models
 messages = [system_msg, human_msg]
-response = model.invoke(messages)  # Returns AIMessage
+response = model.invoke(messages)  
